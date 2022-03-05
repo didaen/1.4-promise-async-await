b8385d75db91ce7fee64494e10603afa4af1af5d
Dida En <firdanb@students.unnes.ac.id>
2022-03-05 14:58:28 +0700

    Belajar membuat Promise, Async, dan Await.

---
+++ script.js
@@ -0,0 +1,25 @@
+let ditepati = true;
+janji1 = new Promise((resolve, reject) => {
+    if( ditepati ) {
+        resolve('Janji ditepati.');
+    } else {
+        reject('Janji diingkari');
+    }
+});
+
+console.log(janji1);
+janji1
+    .then( response => console.log('OK : '+ response) )
+    .catch( response => console.log('NOT OK : ' + response) );
+
+janji2 = new Promise( resolve => {
+    setTimeout(() => {
+        resolve('Janji kedua ditepati');
+    }, 2000);
+});
+
+// TIDAK BISA LANGSUNG BEGINI
+// KARENA YANG DICETAK <PENDING>
+// console.log(janji2);
+
+janji2.then(() => console.log(janji2));
